4e8a20dc3a58cc4c23996b5546a97854ee99d89e
Kleydson Beckman <kleydson_beckman@hotmail.com>
2023-03-13 16:34:47 -0300

    fix(Ajustes): Insert Scripts

--- src/components/BoxText/index.tsx
+++ src/components/BoxText/index.tsx
@@ -7,45 +7,66 @@ class BoxText extends React.Component<any, any> {
 
   constructor(props: any) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    let botao = document.querySelector("#btn");
-    let textarea = (document.getElementById('text') as HTMLInputElement).value;
+  // handleClick() {
+  //   let textarea = (document.getElementById('text') as HTMLInputElement).value;
 
-    if (textarea == "" || textarea == null || textarea == undefined) {
-      Swal.fire({
-        title: '<strong>Você precisa digitar uma instrução para jogar, meu jovem gafanhoto!</strong>',
-        icon: 'warning',
-        showCloseButton: false,
-        showCancelButton: false,
-        focusConfirm: false,
-        iconColor: '#3f5ca7',
-        confirmButtonColor: '#3f5ca7',
-        color: '#30395a',
-      })
-    } else {
-      botao?.addEventListener('click', () => {
-        textarea = textarea.replace(/\s/g, '');
-        var valor = textarea;
-        console.log(valor);
-      })
-    }
-  }
+  //   if (textarea == "" || textarea == null || textarea == undefined) {
+  //     Swal.fire({
+  //       title: '<strong>Você precisa digitar uma instrução para jogar, meu jovem gafanhoto!</strong>',
+  //       icon: 'warning',
+  //       showCloseButton: false,
+  //       showCancelButton: false,
+  //       focusConfirm: false,
+  //       iconColor: '#3f5ca7',
+  //       confirmButtonColor: '#3f5ca7',
+  //       color: '#30395a',
+  //     })
+  //   } else {
+  //     textarea = textarea.replace(/\s/g, '')
+  //     if (textarea == "MOVEATOB") {
+  //       move(0,1)
+  //       console.log('Movi A pra B')
+  //     }
+  //     if (textarea == "MOVEATOC") {
+  //       move(0, 2)
+  //       console.log('Movi A pra C')
+  //     }
+  //     if (textarea == "MOVEBTOC") {
+  //       move(1, 2)
+  //       console.log('Movi B pra C')
+  //     }
+  //     if (textarea == "MOVEBTOA") {
+  //       move(1, 0)
+  //       console.log('Movi B pra A')
+  //     }
+  //     if (textarea == "MOVECTOA") {
+  //       move(3, 0)
+  //       console.log('Movi C pra A')
+  //     }
+  //     if (textarea == "MOVECTOB") {
+  //       move(3, 2)
+  //       console.log('Movi C pra B')
+  //     }
+  //     else {
+  //       alert('esse movimento é proibido')
+  //     }
+  //   }
+  // }
 
 
   render() {
     return (
       <div className="box-text">
         <div className="box-text-area">
-            <textarea className="box-text-input" id="text" placeholder="Enter the instructions..." />
+          <textarea className="box-text-input" id="text" placeholder="Enter the instructions..." />
         </div>
-        <button className="box-text-btn" type='button' id="btn" onClick={this.handleClick}>SUBMIT</button>
+        <button className="box-text-btn" type='button' id="btn2" >SUBMIT</button>
         <BtnModal />
       </div>
     )
   }
 }
 export default BoxText
-
--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import './index.css'
 
+
 const BoxCounter = () => {
     return (
         <div className="box-counter">
             <div className="box-counter-mov">
                 <div className="box-title">Movements Made</div>
-                <div className="box-count">150</div>
+                <div className="box-count">
+
+                </div>
             </div>
             <div className="box-counter-min">
                 <div className="box-count min">150</div>
